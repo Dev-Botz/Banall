6bf84dc92c40e66a0feb6611e5a455b09a0336ec
Dev-Botz <120160209+Dev-Botz@users.noreply.github.com>
2023-04-06 17:28:17 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -17,21 +17,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 if not Config.TELEGRAM_TOKEN:
    bot=Client(api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,session_name=Config.PYRO_SESSION)   
 else:
-   bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)
-
-@bot.on_message(filters.command("banall"))
-async def _(bot, msg):
-    print("getting memebers from {}".format(msg.chat.id))
-    async for i in bot.iter_chat_members(msg.chat.id):
-        try:
-            await bot.ban_chat_member(chat_id =msg.chat.id,user_id=i.user.id)
-            print("kicked {} from {}".format(i.user.id,msg.chat.id))
-        except FloodWait as e:
-            await asyncio.sleep(e.x)
-            print(e)
-        except Exception as e:
-            print(" failed to kicked {} from {}".format(i.user.id,e))           
-    print("process completed")
+   bot=Client(":memory:",api_id=Config.TELEGRAM_APP_ID,api_hash=Config.TELEGRAM_APP_HASH,bot_token=Config.TELEGRAM_TOKEN)    print("process completed")
 
 
 @bot.on_message(filters.command("uban"))
